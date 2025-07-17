2f50ba27a629fde8abbc8103e68289431e9f735b
keller hill <hillkc@umich.edu>
2025-07-17 16:11:06 +0200

    Adding jobs into api

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,15 @@ import AddJobPage from './pages/AddJobPage'
 
 
 const App = () => {
-  const addJob = (newJob) => {
-  console.log(newJob);
+  const addJob = async (newJob) => {
+  const res = await fetch('/api/jobs', {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(newJob)
+  });
+  return;
 };
 
 const router = createBrowserRouter(
